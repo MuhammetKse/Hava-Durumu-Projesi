2f78abcea6674396e27d6f475140621a2cb1151c
Muhammet Köse <umutkose3444@gmail.com>
2023-02-03 13:08:36 +0300

    düzenleme yapıldı 3.02.2023

--- Hava Durumu Projesi/Form1.Designer.cs
+++ Hava Durumu Projesi/Form1.Designer.cs
@@ -40,11 +40,6 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.label15 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -78,16 +73,16 @@
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(12, 124);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 29);
+            this.label3.Size = new System.Drawing.Size(101, 29);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Bugün";
+            this.label3.Text = "TODAY";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(198, 234);
+            this.label5.Location = new System.Drawing.Point(180, 235);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(242, 91);
             this.label5.TabIndex = 3;
@@ -108,29 +103,29 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(431, 218);
+            this.label4.Location = new System.Drawing.Point(451, 218);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 29);
+            this.label4.Size = new System.Drawing.Size(30, 29);
             this.label4.TabIndex = 5;
-            this.label4.Text = "C";
+            this.label4.Text = "K";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(7, 411);
+            this.label6.Location = new System.Drawing.Point(-1, 411);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(95, 29);
+            this.label6.Size = new System.Drawing.Size(72, 29);
             this.label6.TabIndex = 6;
-            this.label6.Text = "Rüzgar";
+            this.label6.Text = "Wind";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(108, 411);
+            this.label7.Location = new System.Drawing.Point(115, 411);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(87, 29);
             this.label7.TabIndex = 7;
@@ -141,7 +136,7 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(108, 470);
+            this.label8.Location = new System.Drawing.Point(115, 485);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(87, 29);
             this.label8.TabIndex = 8;
@@ -152,77 +147,22 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label9.ForeColor = System.Drawing.Color.White;
-            this.label9.Location = new System.Drawing.Point(7, 470);
+            this.label9.Location = new System.Drawing.Point(-1, 485);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(68, 29);
+            this.label9.Size = new System.Drawing.Size(114, 29);
             this.label9.TabIndex = 9;
-            this.label9.Text = "Nem";
+            this.label9.Text = "Humidity";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(209, 345);
+            this.label10.Location = new System.Drawing.Point(191, 345);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(89, 29);
+            this.label10.Size = new System.Drawing.Size(111, 29);
             this.label10.TabIndex = 10;
-            this.label10.Text = "Durum";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label11.ForeColor = System.Drawing.Color.White;
-            this.label11.Location = new System.Drawing.Point(7, 523);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(79, 29);
-            this.label11.TabIndex = 12;
-            this.label11.Text = "Yağış";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label12.ForeColor = System.Drawing.Color.White;
-            this.label12.Location = new System.Drawing.Point(110, 523);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(50, 29);
-            this.label12.TabIndex = 11;
-            this.label12.Text = "0%";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label13.ForeColor = System.Drawing.Color.White;
-            this.label13.Location = new System.Drawing.Point(7, 579);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(55, 29);
-            this.label13.TabIndex = 14;
-            this.label13.Text = "HİS";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label14.ForeColor = System.Drawing.Color.White;
-            this.label14.Location = new System.Drawing.Point(110, 579);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(76, 29);
-            this.label14.TabIndex = 13;
-            this.label14.Text = "00.00";
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label15.ForeColor = System.Drawing.Color.White;
-            this.label15.Location = new System.Drawing.Point(192, 560);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(22, 20);
-            this.label15.TabIndex = 15;
-            this.label15.Text = "C";
+            this.label10.Text = "situation";
             // 
             // button1
             // 
@@ -243,11 +183,6 @@
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(57)))), ((int)(((byte)(119)))));
             this.ClientSize = new System.Drawing.Size(493, 629);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label15);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.label14);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label12);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -283,11 +218,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label14;
-        private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Button button1;
     }
 }
--- Hava Durumu Projesi/Form1.cs
+++ Hava Durumu Projesi/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Net.WebRequestMethods;
 
 namespace Hava_Durumu_Projesi
 {
@@ -20,8 +21,10 @@ namespace Hava_Durumu_Projesi
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+            
             label2.Text = DateTime.Now.ToShortDateString();
-            string connection = "https://api.openweathermap.org/data/2.5/onecall?lat=41.008240&lon=28.978359&units=metric&appid=492547e516c3d2a1d74f214e37c437f3";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&appid=7a92ce2a13a8e0b383dc773223ce2f86";
             XDocument hava = XDocument.Load(connection);
             var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             label5.Text = sicaklik.ToString();
@@ -30,15 +33,14 @@ namespace Hava_Durumu_Projesi
             var nem= hava.Descendants("humidity").ElementAt(0).Attribute("value").Value;
             label8.Text =nem+" %";
             var durum = hava.Descendants("weather").ElementAt(0).Attribute("value").Value;
-            label10.Text =durum;
-             
+            durum = "bulutlu";
             if (durum=="açık")
             {
                 pictureBox1.ImageLocation = @"C:\\Users\\kickb\\OneDrive\\Masaüstü\\sun-7098480_1280.png";
             }
             else if (durum=="bulutlu")
             {
-                pictureBox1.ImageLocation = "C:\\Users\\kickb\\OneDrive\\Masaüstü\\3294621.png";
+                pictureBox1.ImageLocation = "C:\\Users\\kickb\\OneDrive\\Masaüstü\\3295537.png";
             }
             
         }
@@ -46,6 +48,11 @@ namespace Hava_Durumu_Projesi
         private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
